447338363a4200c92f40c23f69bf893213214279
Евгений Иванов <Tyuiu.IvanovEO@outlook.com>
2025-12-08 20:41:25 +0500

    Добавьте файлы проекта.

---
+++ Tyuiu.IvanovEO.Sprint6.Task0.V13.Lib/Class1.cs
@@ -0,0 +1,11 @@
+﻿using tyuiu.cources.programming.interfaces.Sprint6;
+namespace Tyuiu.IvanovEO.Sprint6.Task0.V13.Lib
+{
+    public class Class1 : ISprint6Task0V13
+    {
+        public double Calculate(int x)
+        {
+            return 1;
+        }
+    }
+}
---
+++ Tyuiu.IvanovEO.Sprint6.Task0.V13.Test/Test1.cs
@@ -0,0 +1,13 @@
+﻿using Tyuiu.IvanovEO.Sprint6.Task0.V13;
+namespace Tyuiu.IvanovEO.Sprint6.Task0.V13.Test
+{
+    [TestClass]
+    public sealed class Test1
+    {
+        [TestMethod]
+        public bool TestMethod1()
+        {
+            return true;
+        }
+    }
+}
