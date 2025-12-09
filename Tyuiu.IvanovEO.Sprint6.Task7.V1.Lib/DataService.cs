384859bf7de96d75062ebe40e8842294d3357007
Евгений Иванов <Tyuiu.IvanovEO@outlook.com>
2025-12-10 01:06:39 +0500

    Task7

---
+++ Tyuiu.IvanovEO.Sprint6.Task7.V1.Lib/DataService.cs
@@ -0,0 +1,12 @@
+﻿using tyuiu.cources.programming.interfaces.Sprint6;
+namespace Tyuiu.IvanovEO.Sprint6.Task7.V1.Lib
+{
+    public class DataService : ISprint6Task7V1
+    {
+        public int[,] GetMatrix(string path)
+        {
+            int[,] a = { {1} };
+            return a;
+        }
+    }   
+}
---
+++ Tyuiu.IvanovEO.Sprint6.Task7.V1/Form1.cs
@@ -0,0 +1,10 @@
+namespace Tyuiu.IvanovEO.Sprint6.Task7.V1
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+    }
+}
