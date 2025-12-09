43df06980078b98ecfa7841f64a1e4241c886d48
Евгений Иванов <Tyuiu.IvanovEO@outlook.com>
2025-12-10 00:35:58 +0500

    Task5

---
+++ Tyuiu.IvanovEO.Sprint6.Task5.V23.Lib/Class1.cs
@@ -0,0 +1,12 @@
+﻿using tyuiu.cources.programming.interfaces.Sprint6;
+namespace Tyuiu.IvanovEO.Sprint6.Task5.V23.Lib
+{
+    public class Class1 : ISprint6Task5V23
+    {
+        public double[] LoadFromDataFile(string path)
+        {
+            double[] data = { };
+            return data;
+        }
+    }
+}
