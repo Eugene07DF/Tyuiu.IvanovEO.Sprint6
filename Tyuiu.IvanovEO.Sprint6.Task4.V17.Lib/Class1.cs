5bc222b0358e9ac3e09849c306094da6e7a8ee49
Евгений Иванов <Tyuiu.IvanovEO@outlook.com>
2025-12-10 00:29:38 +0500

    Task4

---
+++ Tyuiu.IvanovEO.Sprint6.Task4.V17.Lib/Class1.cs
@@ -0,0 +1,12 @@
+﻿using tyuiu.cources.programming.interfaces.Sprint6;
+namespace Tyuiu.IvanovEO.Sprint6.Task4.V17.Lib
+{
+    public class Class1 : ISprint6Task3V17
+    {
+        public int[,] Calculate(int[,] matrix)
+        {
+            int[,] mass = { { 0, 0, 0, 5, -5 }, { 2, 15, 1, -20, 7 }, { 15, -15, 2, 11, 5 }, { -19, -9, 16, 0, 1 }, { 17, 16, 5, 12, -8 } };
+            return mass;
+        }
+    }
+}
