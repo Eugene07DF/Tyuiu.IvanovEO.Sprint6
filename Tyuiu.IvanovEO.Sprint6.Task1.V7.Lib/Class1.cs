152cb28759f5cd3dac46511667bfe75fc566a592
Евгений Иванов <Tyuiu.IvanovEO@outlook.com>
2025-12-09 23:31:46 +0500

    Task1

---
+++ Tyuiu.IvanovEO.Sprint6.Task1.V7.Lib/Class1.cs
@@ -0,0 +1,12 @@
+﻿using tyuiu.cources.programming.interfaces.Sprint6;
+namespace Tyuiu.IvanovEO.Sprint6.Task1.V7.Lib
+{
+    public class Class1 : ISprint6Task1V7
+    {
+        public double[] GetMassFunction(int startValue, int stopValue)
+        {
+            double[] mass = { 1.0 };
+            return mass;
+        }
+    }
+}
