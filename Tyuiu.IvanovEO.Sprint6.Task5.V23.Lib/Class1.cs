383325c3fac81367536aff19b7a4892f68a6f637
Евгений Иванов <Tyuiu.IvanovEO@outlook.com>
2025-12-10 00:36:20 +0500

    Task5

--- Tyuiu.IvanovEO.Sprint6.Task5.V23.Lib/Class1.cs
+++ Tyuiu.IvanovEO.Sprint6.Task5.V23.Lib/Class1.cs
@@ -5,7 +5,7 @@ namespace Tyuiu.IvanovEO.Sprint6.Task5.V23.Lib
     {
         public double[] LoadFromDataFile(string path)
         {
-            double[] data = { };
+            double[] data = { -17.0, -14.32, -7.84, -1.57, -3.64, -13.26, -8.91, -17.77, -9.0, -1.49, -7.0 };
             return data;
         }
     }
