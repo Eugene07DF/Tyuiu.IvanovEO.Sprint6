f393e197320ccc06f920adf4b8ac4c85052720d4
Евгений Иванов <Tyuiu.IvanovEO@outlook.com>
2025-12-09 23:45:09 +0500

    Task2

--- Tyuiu.IvanovEO.Sprint6.Task2.V3.Lib/Class1.cs
+++ Tyuiu.IvanovEO.Sprint6.Task2.V3.Lib/Class1.cs
@@ -5,7 +5,7 @@ namespace Tyuiu.IvanovEO.Sprint6.Task2.V3.Lib
     {
         public double[] GetMassFunction(int startValue, int stopValue)
         {
-            double[] mass = { 7.76, 7.36, 7.26, 7.9, 15.88, 2.0, 4.35, 5.63, 6.49, 6.49, 6.32 };
+            double[] mass = { -12.18, 16.03, 18.87, 4.96, -9.99, -2.0, 2.16, -7.54, -22.76, -20.45, 7.77 };
             return mass;
         }
     }
