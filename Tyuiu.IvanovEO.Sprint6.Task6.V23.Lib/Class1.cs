49478464b84af6ab04bf8226473835c0f2194006
Евгений Иванов <Tyuiu.IvanovEO@outlook.com>
2025-12-10 00:42:28 +0500

    Task6

---
+++ Tyuiu.IvanovEO.Sprint6.Task6.V23.Lib/Class1.cs
@@ -0,0 +1,11 @@
+﻿using tyuiu.cources.programming.interfaces.Sprint6;
+namespace Tyuiu.IvanovEO.Sprint6.Task6.V23.Lib
+{
+    public class Class1 : ISprint6Task6V23
+    {
+        public string CollectTextFromFile(string path)
+        {
+            return "опа";
+        }
+    }
+}
---
+++ Tyuiu.IvanovEO.Sprint6.Task6.V23.Test/Test1.cs
@@ -0,0 +1,12 @@
+﻿
+namespace Tyuiu.IvanovEO.Sprint6.Task6.V23.Test
+{
+    [TestClass]
+    public sealed class Test1
+    {
+        [TestMethod]
+        public void TestMethod1()
+        {
+        }
+    }
+}
