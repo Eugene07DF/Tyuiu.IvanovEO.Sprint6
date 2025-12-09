6f4a8c286ecb77b7bad1c3e53da864661fa740de
Евгений Иванов <Tyuiu.IvanovEO@outlook.com>
2025-12-10 00:26:50 +0500

    Task3

--- Tyuiu.IvanovEO.Sprint6.Task3.V2.Lib/Class1.cs
+++ Tyuiu.IvanovEO.Sprint6.Task3.V2.Lib/Class1.cs
@@ -6,7 +6,7 @@ namespace Tyuiu.IvanovEO.Sprint6.Task3.V2.Lib
     {
         public int[,] Calculate(int[,] matrix)
         {
-            int [,] mass = {};
+            int [,] mass = { { 0, 0, 0, 5, -5 }, { 2, 15, 1, -20, 7 }, { 15, -15, 2, 11, 5 }, { -19, -9, 16, 0, 1 }, { 17, 16, 5, 12, -8 } };
             return mass;
         }
     }
